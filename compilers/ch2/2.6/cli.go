45e568da23a562ee1def5bce601ae2e8d0033ee2
Amisi Kiarie <8081220+akiarie@users.noreply.github.com>
2022-01-01 13:29:41 +0300

    2.6

--- compilers/ch2/2.6/cli.go
+++ compilers/ch2/2.6/cli.go
@@ -145,47 +145,58 @@ func parsetoken(input string, pos int) (*token, int, error) {
 	}
 	// ignore comments
 	if st+1 < len(input) && input[st] == '/' {
-		prevst := st
-		st += 1
-		if input[st] == '/' {
-			for st += 1; st < len(input); st++ {
-				if input[st] == '\n' {
-					st++ // skip newline
-					tk, shift, err := parsetoken(input, st)
+		i := st + 1
+		if input[i] == '/' {
+			for i += 1; i < len(input); i++ {
+				if input[i] == '\n' {
+					i++ // skip newline
+					tk, shift, err := parsetoken(input, i)
 					if err != nil {
 						return nil, -1, err
 					}
-					return tk, (st - pos) + shift, nil
+					return tk, (i - pos) + shift, nil
 				}
 			}
-		} else if input[st] == '*' {
-			for st += 1; st+1 < len(input); st++ {
-				if input[st:st+2] == "*/" {
-					st += 2 // input[st+1] == '/'
-					tk, shift, err := parsetoken(input, st)
+			return nil, -1, fmt.Errorf("Un-terminated comment %q", input[st:i])
+		} else if input[i] == '*' {
+			for i += 1; i+1 < len(input); i++ {
+				if input[i:i+2] == "*/" {
+					i += 2 // input[i+1] == '/'
+					tk, shift, err := parsetoken(input, i)
 					if err != nil {
 						return nil, -1, err
 					}
-					return tk, (st - pos) + shift, nil
+					return tk, (i - pos) + shift, nil
 				}
 			}
+			return nil, -1, fmt.Errorf("Un-terminated comment %q", input[st:i])
 		}
-		return nil, -1, fmt.Errorf("Un-terminated comment %q", input[prevst:st])
 	}
 
 	if st >= len(input) {
 		return &token{class: tkSpace, value: tkSpace, lexeme: input[pos:]}, len(input[pos:]), nil
 	}
 
-	if unicode.IsDigit(rune(input[st])) {
+	if dotted := input[st] == '.'; dotted || unicode.IsDigit(rune(input[st])) {
 		i := st
 		for ; i < len(input); i++ {
-			if !unicode.IsDigit(rune(input[i])) {
+			if input[i] == '.' {
+				if i > st && dotted {
+					return nil, -1, fmt.Errorf("Double-point in %q", input[st:i+1])
+				}
+				dotted = true
+			} else if !unicode.IsDigit(rune(input[i])) {
 				break
 			}
 		}
-		if val, err := strconv.ParseUint(input[st:i], 10, 64); err == nil {
-			return &token{class: tkNum, lexeme: input[pos:i], value: fmt.Sprintf("%d", val)}, len(input[pos:i]), nil
+		if dotted {
+			if f, err := strconv.ParseFloat(input[st:i], 64); err == nil {
+				return &token{class: tkNum, lexeme: input[pos:i], value: fmt.Sprintf("%.2f", f)}, len(input[pos:i]), nil
+			}
+		} else {
+			if u, err := strconv.ParseUint(input[st:i], 10, 64); err == nil {
+				return &token{class: tkNum, lexeme: input[pos:i], value: fmt.Sprintf("%d", u)}, len(input[pos:i]), nil
+			}
 		}
 	}
 
@@ -199,10 +210,17 @@ func parsetoken(input string, pos int) (*token, int, error) {
 		return &token{class: tkId, lexeme: input[pos:i], value: input[st:i]}, len(input[pos:i]), nil
 	}
 
-	isop := func(c byte) bool { return strings.IndexByte("+-*/", c) != -1 }
-	if isop(input[st]) {
+	if strings.IndexByte("+-*/", input[st]) != -1 {
 		cs := fmt.Sprintf("%c", input[st])
 		return &token{class: tkOp, lexeme: cs, value: cs}, 1 + st - pos, nil
+	} else if strings.IndexByte("<>!=", input[st]) != -1 {
+		if st+1 < len(input) && input[st+1] == '=' {
+			return &token{class: tkOp, lexeme: input[st : st+2], value: input[st : st+2]}, 2 + st - pos, nil
+		}
+		if input[st] == '<' || input[st] == '>' {
+			cs := fmt.Sprintf("%c", input[st])
+			return &token{class: tkOp, lexeme: cs, value: cs}, 1 + st - pos, nil
+		}
 	}
 
 	if input[st] == '(' {
